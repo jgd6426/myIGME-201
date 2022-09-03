7404fbd2c151afb86a9f03099531d44283466ad3
Judy Derrick <jgd6426@rit.edu>
2022-09-03 17:01:37 -0400

    PE4

--- PE4-2/Program.cs
+++ PE4-2/Program.cs
@@ -19,19 +19,21 @@ namespace PE4_2
             //          but rejects any input where both numbers are greater than 10
             //          and asks for new numbers
             // Restrictions: None
-            int var1;
-            int var2;
 
+            // get first number from user and display
             Console.WriteLine("Please enter a number: ");
-            var1 = Convert.ToInt32(Console.ReadLine());
+            int var1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your first number is " + var1);
 
+            // get second number from user and display
             Console.WriteLine("Please enter a second number: ");
-            var2 = Convert.ToInt32(Console.ReadLine());
+            int var2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your second number is " + var2);
 
+            // check if both are greater than 10
             if (var1 > 10 && var2 > 10)
             {
+                // if they are keep asking for new numbers until at least one is less than 10
                 while (var1 > 10 && var2 > 10)
                 {
                     Console.WriteLine("Oops! Sorry but both numbers can't be greater than ten. Try again.");
