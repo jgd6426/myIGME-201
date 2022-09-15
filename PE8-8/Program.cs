48939e5e8b0ea109c1cc2b31e969e4afdb875261
Judy <100483381+jgd6426@users.noreply.github.com>
2022-09-14 22:23:12 -0400

    Add files via upload

---
+++ PE8-8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_8
+{
+    // Class: IGME 201.01
+    // Author: Judy Derrick
+    // Purpose: PE8 Q8 - More About Variables
+    // Restrictions: None
+    internal class Program
+    {
+        // Method: Main
+        // Purpose: write a console application that accepts a string
+        //          and replaces all occurances of the word "no" with "yes"
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write something with 'yes' or 'no': ");
+            string userWords = Console.ReadLine();
+
+            string newWords = userWords.Replace("no", "yes");
+            Console.WriteLine(newWords);
+        }
+    }
+}
