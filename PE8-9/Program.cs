74ac16b2e169fc7e0020911885f8b8f24136bf00
Judy <100483381+jgd6426@users.noreply.github.com>
2022-09-14 22:31:38 -0400

    Add files via upload

---
+++ PE8-9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_9
+{
+    // Class: IGME 201.01
+    // Author: Judy Derrick
+    // Purpose: PE8 Q9 - More About Variables
+    // Restrictions: None
+    internal class Program
+    {
+        // Method: Main
+        // Purpose: write a console application that places double quotes
+        //          around each word in a string
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type some words: ");
+            string userWords = Console.ReadLine();
+
+            string newWords = "\"" + userWords;
+            newWords = newWords.Replace(" ", "\" \"");
+            newWords += "\"";
+
+            Console.WriteLine(newWords);
+        }
+    }
+}
