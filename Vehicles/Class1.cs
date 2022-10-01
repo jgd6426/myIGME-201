3e95200a5df5d81a4e22c760ef29a042be26b238
Judy <100483381+jgd6426@users.noreply.github.com>
2022-09-30 22:50:20 -0400

    Vehicles

---
+++ Vehicles/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+    public abstract class Car : Vehicle { }
+
+    public interface PassengerCarrier
+    {
+        void LoadPassenger();
+    }
+
+    public interface HeavyLoadCarrier { }
+
+    public abstract class Train : Vehicle { }
+
+    public class Compact : Car
+    {
+        public override void LoadPassenger() { }
+    }
+
+    public class SUV : Car
+    {
+        public override void LoadPassenger() { }
+    }
+
+    public class Pickup : Car
+    {
+        public override void LoadPassenger() { }
+        public interface HeavyLoadCarrier { }
+
+    }
+
+    public class PassengerTrain : Train
+    {
+        public override void LoadPassenger() { }
+    }
+
+    public class FreightTrain : Train
+    {
+        public interface HeavyLoadCarrier { }
+    }
+
+    public class _424DoubleBogey : Train
+    {
+        public interface HeavyLoadCarrier { }
+    }
+
+}
