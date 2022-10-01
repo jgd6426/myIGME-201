e77b5daa4366b9fdd15e4b7814fd714f9ac79b4e
Judy <100483381+jgd6426@users.noreply.github.com>
2022-09-30 23:22:55 -0400

    Traffic

---
+++ Traffic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    static internal class Program
+    {
+        interface IPassengerCarrier
+        {
+            void LoadPassenger();
+        }
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void AddPassenger(IPassengerCarrier vehicleObject) 
+        {
+            vehicleObject.LoadPassenger();
+
+            Console.WriteLine(vehicleObject.ToString());
+        }
+    }
+}
