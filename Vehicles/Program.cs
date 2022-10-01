2d22de4055eaae4c0daf541968d68c15dc092b18
Judy <100483381+jgd6426@users.noreply.github.com>
2022-09-30 22:32:05 -0400

    Add files via upload

---
+++ Vehicles/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    internal class Program
+    {
+        public abstract class Vehicle
+        {
+            public virtual void LoadPassenger() { }
+        }
+        public abstract class Car: Vehicle { }
+
+        public interface PassengerCarrier
+        {
+            void LoadPassenger();
+        }
+
+        public interface HeavyLoadCarrier { }
+
+        public abstract class Train: Vehicle { }
+
+        public class Compact: Car
+        {
+            public override void LoadPassenger() { }
+        }
+
+        public class SUV: Car
+        {
+            public override void LoadPassenger() { }
+        }
+
+        public class Pickup : Car
+        {
+            public override void LoadPassenger() { }
+            public interface HeavyLoadCarrier { }
+            
+        }
+
+        public class PassengerTrain: Train
+        {
+            public override void LoadPassenger() { }
+        }
+
+        public class FreightTrain: Train
+        {
+            public interface HeavyLoadCarrier { }
+        }
+
+        public class _424DoubleBogey: Train
+        {
+            public interface HeavyLoadCarrier { }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
