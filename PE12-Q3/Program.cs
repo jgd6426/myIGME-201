117f1690a506399666c853805b298f35a3eb1f0b
Judy <100483381+jgd6426@users.noreply.github.com>
2022-10-10 15:48:41 -0400

    P12-Q3

---
+++ PE12-Q3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PE12_Q3
+{
+    internal class Program
+    {
+        public class MyClass
+        {
+            private string myString = "myString";
+
+            public string MyString;
+
+            public virtual string GetString()
+            {
+                {
+                    return myString;
+                }
+            }
+        }
+
+        public class MyDerivedClass: MyClass
+        {
+            public override string GetString()
+            {
+                return MyString + base.GetString();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MyDerivedClass myDClass = new MyDerivedClass();
+            Console.WriteLine(myDClass.GetString());
+        }
+    }
+}
