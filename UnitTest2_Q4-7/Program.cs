9b91719dd588ac53a1e75375b310cb3f83781df4
Judy <100483381+jgd6426@users.noreply.github.com>
2022-10-23 14:57:23 -0400

    Q4-7 UnitTest2

---
+++ UnitTest2_Q4-7/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest2_Q4_7
+{
+    public interface PhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+    }
+
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set {;  }
+        }
+
+        public abstract void Connect();
+
+        public abstract void Disconnect();
+    }
+
+    public class RotaryPhone: Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+        public void MakeCall()
+        {
+            // Console.WriteLine("Ring ring ring...");
+        }
+        public void HangUp()
+        {
+            // Console.WriteLine("Bye bye");
+        }
+        public override void Connect()
+        {
+
+        }
+        public override void Disconnect()
+        {
+
+        }   
+    }
+
+    public class PushButtonPhone: Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+        public void MakeCall()
+        {
+            // Console.WriteLine("Ring ring ring...");
+        }
+        public void HangUp()
+        {
+            // Console.WriteLine("Bye bye");
+        }
+        public override void Connect()
+        {
+
+        }
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public class Tardis: RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho
+        {
+            get { return whichDrWho; }
+        }
+        public string FemaleSideKick
+        {
+            get { return femaleSideKick; }
+        }
+        public void TimeTravel()
+        {
+            // Console.WriteLine("Whoosh whoosh");
+        }
+        public static bool operator < (Tardis dr1, Tardis dr2)
+        {
+            return (dr1.whichDrWho < dr2.whichDrWho);
+        }
+        public static bool operator <= (Tardis dr1, Tardis dr2)
+        {
+            return (dr1.whichDrWho <= dr2.whichDrWho);
+        }
+        public static bool operator >(Tardis dr1, Tardis dr2)
+        {
+            return (dr1.whichDrWho > dr2.whichDrWho);
+        }
+        public static bool operator >= (Tardis dr1, Tardis dr2)
+        {
+            return (dr1.whichDrWho >= dr2.whichDrWho);
+        }
+        public static bool operator == (Tardis dr1, Tardis dr2)
+        {
+            if (dr1.whichDrWho == 10)
+            {
+                return dr1 > dr2;
+            }
+            else if (dr2.whichDrWho == 10)
+            {
+                return dr2 > dr1;
+            }
+            else
+            {
+                return (dr1.whichDrWho == dr2.whichDrWho);
+            }          
+        }
+        public static bool operator != (Tardis dr1, Tardis dr2)
+        {
+            return (dr1.whichDrWho != dr2.whichDrWho);
+        }
+    }
+
+    public class PhoneBooth: PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor()
+        {
+            Console.WriteLine("door opened");
+        }
+        public void CloseDoor()
+        {
+            
+        }
+    }
+    static internal class Program
+    {
+        static void UsePhone(object obj)
+        {
+            
+
+            Console.WriteLine("hi");
+            obj.MakeCall();
+
+            obj.HangUp();
+
+            // if obj = PhoneBooth --> OpenDoor()
+            if (obj is PhoneBooth)
+            {
+               obj.OpenDoor();
+            }
+
+            // if obj = Tardis --> TimeTravel()
+            else if (obj is Tardis)
+            {
+                obj.TimeTravel();
+            }
+        }
+        static void Main(string[] args)
+        {
+            // create a Tardis object
+            Tardis tardis = new Tardis();
+
+            // create a PhoneBooth object
+            PhoneBooth phoneBooth = new PhoneBooth();
+
+            // pass each to UsePhone(object obj)
+            UsePhone(tardis);
+            UsePhone(phoneBooth);
+        }
+    }
+}
