06e22116fd39cb07df382f74541faa06d1f7fabc
Judy <100483381+jgd6426@users.noreply.github.com>
2022-12-05 22:27:26 -0500

    FinalExam #1

---
+++ MyStack/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    public class MyStack
+    {
+        public List<int> myStack = new List<int>();
+
+        public void Push(int n)
+        {
+            myStack.Add(n);
+        }
+
+        public int? Peek()
+        {
+            if (myStack.Count > 0)
+            {
+                return myStack[myStack.Count - 1];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public int? Pop()
+        {
+            if (myStack.Count > 0)
+            {
+                int? r = myStack[myStack.Count - 1];
+                myStack.RemoveAt(myStack.Count - 1);
+                return r;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
